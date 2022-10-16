e4f79ffc41ba250d536314826aa6c1b38800062d
royanlord <royanfarid32@gmail.com>
2022-10-16 14:45:18 +0700

    add code to the script.js where menu on the navbar can autoscroll to a section when clicked

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ function focusNav(elementTag) {
 
   elementTag.classList.add('aktif');
 }
+
+// make autoscroll to certain sections when menu on the navbar is clicked
+$(document).ready(function(){
+  $('.header').height($(window).height());
+ 
+  $(".navbar a").click(function(){
+      $("body,html").animate({
+          scrollTop:$("#" + $(this).data('value')).offset().top
+      },1000)
+   
+  })
+})
